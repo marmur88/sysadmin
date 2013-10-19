920062f231cbcc0a729f88bef1c31715a86513f9
Michal Stusio <stusiek88@gmail.com>
2013-10-19 12:22:51 +0200

    Cookbook postfix added #9

---
+++ cookbooks/postfix/metadata.rb
@@ -0,0 +1,7 @@
+name             'postfix'
+maintainer       'Michal Stusio'
+maintainer_email 'stusiek88@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures postfix'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/postfix/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: postfix
+# Recipe:: default
+#
+# Copyright 2013, Michal Stusio
+#
+# All rights reserved - Do Not Redistribute
+#
+package "postfix"
+
+package "sendemail"
