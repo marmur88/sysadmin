6ec3318101889f1133b1058a86046a21efdaff63
Michal Stusio <stusiek88@gmail.com>
2013-10-21 17:08:48 +0200

    Prepered for new configuration #11

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -6,20 +6,6 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-
-file "/home/michal/test_chef" do
-  action :create
-  owner "michal"
-  mode "0644"
-  content "test2\n"
-end
-
-cookbook_file "/home/michal/cookbook_test" do
-  owner "michal"
-  mode "0644"
-  source "file"
-end
-
 cookbook_file "/home/michal/.screenrc" do
   owner "michal"
   mode "0644"
